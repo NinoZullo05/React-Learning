95efa3644b425c986a87cc7d66b89e36a91347ae
Nino Zullo <nino.zullo05dev@gmail.com>
2024-09-06 18:38:36 +0200

    created 01 : How to create a basic component

---
+++ 01-create-a-basic-component/src/components/first-component.jsx
@@ -0,0 +1,9 @@
+// Example component. We can also create a component using function syntax.
+const FirstComponent = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+    </>
+  );
+};
+export default FirstComponent;
